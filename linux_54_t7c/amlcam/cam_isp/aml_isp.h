c0ce0bd29f5a29a6a02f5aa28bd0ef204066bb5a
xiaoxin.cao <xiaoxin.cao@amlogic.com>
2022-07-27 19:46:52 +0800

    isp: drv support android compile [1/1]
    PD#SWPL-89251
    
    Problem:
    Driver support android compile
    
    Solution:
    Driver Support android compile
    
    Verify:
    local
    
    Change-Id: If6119aab014d2223f7a7c7c7cae3ce7f60961e52
    Signed-off-by: xiaoxin.cao <xiaoxin.cao@amlogic.com>

---
+++ linux_54_t7c/amlcam/cam_isp/aml_isp.h
@@ -0,0 +1,166 @@
+/*
+*
+* SPDX-License-Identifier: GPL-2.0
+*
+* Copyright (C) 2020 Amlogic or its affiliates
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; version 2.
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+* for more details.
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*
+*/
+
+#ifndef __AML_ISP_H__
+#define __AML_ISP_H__
+
+#include <linux/interrupt.h>
+#include <media/v4l2-subdev.h>
+#include <media/v4l2-device.h>
+
+#include "aml_common.h"
+#include "aml_misc.h"
+
+enum {
+	AML_ISP_PAD_SINK_VIDEO,
+	AML_ISP_PAD_SINK_PATTERN,
+	AML_ISP_PAD_SINK_DDR,
+	AML_ISP_PAD_SINK_PARAM,
+	AML_ISP_PAD_SOURCE_STATS,
+	AML_ISP_PAD_SOURCE_STREAM_0,
+	AML_ISP_PAD_SOURCE_STREAM_1,
+	AML_ISP_PAD_SOURCE_STREAM_2,
+	AML_ISP_PAD_SOURCE_STREAM_3,
+	AML_ISP_PAD_SOURCE_STREAM_RAW,
+	AML_ISP_PAD_MAX
+};
+
+enum {
+	AML_ISP_STREAM_DDR,
+	AML_ISP_STREAM_PARAM,
+	AML_ISP_STREAM_STATS,
+	AML_ISP_STREAM_0,
+	AML_ISP_STREAM_1,
+	AML_ISP_STREAM_2,
+	AML_ISP_STREAM_3,
+	AML_ISP_STREAM_RAW,
+	AML_ISP_STREAM_MAX
+};
+
+enum
+{
+	AML_MIRROR_NONE,
+	AML_MIRROR_HORIZONTAL,
+	AML_MIRROR_VERTICAL,
+	AML_MIRROR_BOTH
+};
+
+enum {
+	AML_ISP_RAW_RGGB = 0,
+	AML_ISP_RAW_GRBG,
+	AML_ISP_RAW_GBRG,
+	AML_ISP_RAW_BGGR,
+};
+
+struct isp_dev_t {
+	u32 index;
+	u32 irq;
+	spinlock_t irq_lock;
+	struct tasklet_struct irq_tasklet;
+	void *emb_dev;
+	void __iomem *base;
+	u32 phy_base;
+	u32 apb_dma;
+	u32 slice;
+	struct clk *isp_clk;
+	struct device *dev;
+	struct platform_device *pdev;
+
+	struct aml_format fmt;
+	struct aml_format lfmt;
+	struct aml_format rfmt;
+	u32 wreg_cnt;
+	u32 frm_cnt;
+	u32 irq_status;
+	u32 isp_status;
+	u32 enWDRMode;
+
+	char *bus_info;
+	struct media_pipeline pipe;
+	struct v4l2_device *v4l2_dev;
+	struct v4l2_subdev sd;
+	struct media_pad pads[AML_ISP_PAD_MAX];
+	struct v4l2_mbus_framefmt pfmt[AML_ISP_PAD_MAX];
+	u32 fmt_cnt;
+	const struct aml_format *formats;
+	struct aml_subdev subdev;
+	struct v4l2_ctrl_handler ctrls;
+	struct v4l2_ctrl *wdr;
+
+	spinlock_t wreg_lock;
+	struct aml_buffer rreg_buff;
+	struct aml_buffer wreg_buff;
+	struct aml_buffer ptnr_buff;
+	struct aml_buffer mcnr_buff;
+
+	const struct isp_dev_ops *ops;
+	const struct emb_ops_t *emb_ops;
+    struct aml_slice aslice[3];
+	struct aml_video video[AML_ISP_STREAM_MAX];
+};
+
+struct isp_dev_ops {
+	void (*hw_init)(struct isp_dev_t *isp_dev);
+	void (*hw_reset)(struct isp_dev_t *isp_dev);
+	u32 (*hw_version)(struct isp_dev_t *isp_dev);
+	u32 (*hw_interrupt_status)(struct isp_dev_t *isp_dev);
+	int (*hw_set_input_fmt)(struct isp_dev_t *isp_dev, struct aml_format *fmt);
+	int (*hw_set_slice_fmt)(struct isp_dev_t *isp_dev, struct aml_format *fmt);
+	int (*hw_cfg_slice)(struct isp_dev_t *isp_dev, int pos);
+	int (*hw_set_wdr_mode)(struct isp_dev_t *isp_dev, int wdr_en);
+	int (*hw_cfg_pattern)(struct isp_dev_t *isp_dev, struct aml_format *fmt);
+	int (*hw_stream_set_fmt)(struct aml_video *video, struct aml_format *fmt);
+	int (*hw_stream_cfg_buf)(struct aml_video *video, struct aml_buffer *buff);
+	int (*hw_stream_bilateral_cfg)(struct aml_video *video, struct aml_buffer *buff);
+	void (*hw_stream_crop)(struct aml_video *video);
+	void (*hw_stream_on)(struct aml_video *video);
+	void (*hw_stream_off)(struct aml_video *video);
+	void (*hw_start)(struct isp_dev_t *isp_dev);
+	void (*hw_stop)(struct isp_dev_t *isp_dev);
+	int (*hw_enable_wrmif)(struct aml_video *video, int enable);
+	int (*hw_get_wrmif_stat)(struct aml_video *video);
+	int (*hw_set_rot)(struct aml_video *video, int enable);
+	u64 (*hw_timestamp)(struct isp_dev_t *isp_dev);
+	int (*hw_cfg_ptnr_mif_buf)(struct isp_dev_t *isp_dev, struct aml_buffer *buff);
+	int (*hw_enable_ptnr_mif)(struct isp_dev_t *isp_dev, u32 enable);
+	int (*hw_cfg_mcnr_mif_buf)(struct isp_dev_t *isp_dev, struct aml_format *fmt, struct aml_buffer *buff);
+	int (*hw_enable_mcnr_mif)(struct isp_dev_t *isp_dev, int enable);
+	int (*hw_start_apb_dma)(struct isp_dev_t *isp_dev);
+	int (*hw_check_done_apb_dma)(struct isp_dev_t *isp_dev);
+	int (*hw_manual_trigger_apb_dma)(struct isp_dev_t *isp_dev);
+	int (*hw_fill_rreg_buff)(struct isp_dev_t *isp_dev);
+};
+
+
+int isp_subdev_resume(struct isp_dev_t *isp_dev);
+void isp_subdev_suspend(struct isp_dev_t *isp_dev);
+
+int aml_isp_subdev_init(void *c_dev);
+void aml_isp_subdev_deinit(void *c_dev);
+
+int aml_isp_video_register(struct isp_dev_t *isp_dev);
+void aml_isp_video_unregister(struct isp_dev_t *isp_dev);
+
+int aml_isp_subdev_register(struct isp_dev_t *isp_dev);
+void aml_isp_subdev_unregister(struct isp_dev_t *isp_dev);
+
+extern const struct isp_dev_ops isp_hw_ops;
+extern const struct emb_ops_t emb_hw_ops;
+
+#endif	/* __AML_P1_ISP_H__ */
---
+++ linux_54_t7c/amlcam/cam_isp/hw/t7_isp_tnr.c
@@ -0,0 +1,133 @@
+/* SPDX-License-Identifier: GPL-2.0
+*
+* Copyright (C) 2020 Amlogic or its affiliates
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; version 2.
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+* for more details.
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*
+*/
+
+#include "aml_isp_reg.h"
+#include "aml_isp_hw.h"
+
+static void tnr_cfg_noise_profile(struct isp_dev_t *isp_dev, void *tnr)
+{
+	int i = 0;
+	u32 val = 0;
+	aisp_tnr_cfg_t *t_cfg = tnr;
+
+	for (i = 0; i < 8; i++) {
+		val = (t_cfg->ma_np_lut16[i * 2 + 0] << 0) |
+			(t_cfg->ma_np_lut16[i * 2 + 1] << 12);
+
+		isp_reg_write(isp_dev, ISP_TNR_NP_LUT_0 + i * 4, val);
+	}
+}
+
+static void tnr_cfg_param(struct isp_dev_t *isp_dev, void *param)
+{
+	int i = 0;
+	u32 val = 0;
+	aisp_tnr_cfg_t *tnr_cfg = param;
+
+	val = tnr_cfg->ma_tnr_sad_cor_np_gain;
+	isp_reg_update_bits(isp_dev, ISP_CUBICT_SAD_CORING_NP_CRTL, val, 8, 8);
+
+	val = tnr_cfg->ma_tnr_sad_cor_np_ofst;
+	isp_reg_update_bits(isp_dev, ISP_CUBICT_SAD_CORING_NP_CRTL, val, 0, 8);
+
+	isp_reg_write(isp_dev, ISP_CUBICT_ALPHA0_LUT_ADDR, 0);
+	for (i = 0; i < 16; i++) {
+		val = (tnr_cfg->lut_meta_sad_2alpha[i * 4 + 0] << 0) |
+			(tnr_cfg->lut_meta_sad_2alpha[i * 4 + 1] << 8) |
+			(tnr_cfg->lut_meta_sad_2alpha[i * 4 + 2] << 16) |
+			(tnr_cfg->lut_meta_sad_2alpha[i * 4 + 3] << 24);
+		isp_reg_write(isp_dev, ISP_CUBICT_ALPHA0_LUT_DATA, val);
+	}
+
+	val = (tnr_cfg->ma_mix_h_th_gain[0] << 18) |
+		(tnr_cfg->ma_mix_h_th_gain[1] << 12) |
+		(tnr_cfg->ma_mix_h_th_gain[2] << 6) |
+		(tnr_cfg->ma_mix_h_th_gain[3] << 0);
+	isp_reg_write(isp_dev, ISP_CUBICT_MIX_H_GAIN, val);
+}
+
+static void tnr_cfg_nr(struct isp_dev_t *isp_dev, void *nr)
+{
+	aisp_nr_cfg_t *nr_cfg = nr;
+
+	if (nr_cfg->pvalid.aisp_tnr)
+		tnr_cfg_noise_profile(isp_dev, &nr_cfg->tnr_cfg);
+
+	if (nr_cfg->pvalid.aisp_tnr)
+		tnr_cfg_param(isp_dev, &nr_cfg->tnr_cfg);
+}
+
+void isp_tnr_cfg_param(struct isp_dev_t *isp_dev, struct aml_buffer *buff)
+{
+	aisp_param_t *param = buff->vaddr[AML_PLANE_A];
+
+	if (param->pvalid.aisp_nr)
+		tnr_cfg_nr(isp_dev, &param->nr_cfg);
+}
+
+void isp_tnr_init(struct isp_dev_t *isp_dev)
+{
+	u32 val = 0;
+
+	isp_reg_update_bits(isp_dev, ISP_CUBIC_RAD_CRTL, 1, 26, 1);
+
+	val = (30 << 16) | (40 << 8) | (50 << 0);
+	isp_reg_write(isp_dev, ISP_CUBICT_HIGH_MIX_THRD_Y, val);
+
+	val = (36 << 16) | (5 << 8) | (5 << 0);
+	isp_reg_write(isp_dev, ISP_CUBICT_MIX_THRD_X, val);
+
+	val = (30 << 16) | (50 << 8) | (60 << 0);
+	isp_reg_write(isp_dev, ISP_CUBICT_LOW_MIX_THRD_Y, val);
+
+	val = (80 << 20) | (80 << 8) | (64 << 0);
+	isp_reg_write(isp_dev, ISP_CUBICT_ERR_MIX_RATIO, val);
+
+	val = (70 << 16) | (75 << 4);
+	isp_reg_write(isp_dev, ISP_CUBICT_SAD_LUMA_GAIN_X, val);
+
+	val = (32 << 24) | (24 << 18) | (18 << 12) | (16 << 6) | (16 << 0);
+	isp_reg_write(isp_dev, ISP_CUBICT_SAD_LUMA_GAIN_Y, val);
+
+	isp_reg_update_bits(isp_dev, ISP_CUBICT_SAD_CORING_NP_CRTL, 10, 8, 8);
+
+	val = (240 << 0) | (120 << 8) | (64 << 16) | (48 << 24);
+	isp_reg_write(isp_dev, ISP_LOSS_CORING_META2ALP_0, val);
+
+	val = (32 << 0) | (24 << 8) | (16 << 16) | (16 << 24);
+	isp_reg_write(isp_dev, ISP_LOSS_CORING_META2ALP_1, val);
+
+	isp_reg_write(isp_dev, ISP_CUBICT_DETAIL_CORING, 32);
+
+	isp_reg_update_bits(isp_dev, ISP_CUBICT_ALPHA_LUMA_GAIN_BLC, 240, 0, 12);
+
+	val = (1 << 6) | (0 << 8) | (1 << 12) | (12 << 16) | (8 << 20);
+	isp_reg_write(isp_dev, ISP_CUBICT_META_PP2, val);
+
+	isp_reg_write(isp_dev, ISP_CUBICT_ALPHA0_LUT_DATA, 0);
+
+	val = (1 << 21);
+	isp_reg_write(isp_dev, ISP_CUBICT_MIX_CTRL, val);
+
+	isp_reg_write(isp_dev, ISP_CUBICT_ADP_DTL_NFL, 80);
+
+	val = (2 << 24) | (8 << 16) | (12 << 8);
+	isp_reg_write(isp_dev, ISP_CUBICT_DETAIL_CURV_Y, val);
+
+	isp_reg_update_bits(isp_dev, ISP_PST_TNR_ALP_LUT_1, 36, 16, 6);
+	isp_reg_update_bits(isp_dev, ISP_PST_TNR_ALP_LUT_1, 40, 24, 6);
+}
